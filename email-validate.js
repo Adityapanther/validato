458bbcc1b42ebef1c28ddd6917f0846822099b3a
raj68518 <raj68518@gmail.com>
2020-11-26 16:26:32 +0530

    some code changed

--- email-validate.js
+++ email-validate.js
@@ -1,10 +1,42 @@
-class emailValidato{
 
-    hello(){
-        console.log("hello");
-        
+class emailValidato{
+    emailId;
+
+     data = [];
+    
+    isNotEmpty(){
+        if (this.emailId != "" && this.emailId != "undefined") {
+            this.data.push(true);
+        }
+        this.data.push(false);
     }
 
+    minLength(length){
+        if (this.emailId.length >= length) {
+            return true;
+        }
+        return false;
+    }
+
+    maxLength(length){
+        if (this.emailId.length <= length) {
+            return true;
+        }
+        return false;
+    }
+
+    validate(emailId) {
+        this.emailId = emailId;
+        this.data.forEach(e => {
+            if (e === true) {
+                console.log("hi");
+                
+                return true
+            } else {
+                return false
+            }
+        })
+    }
 }
 
 module.exports = emailValidato;
--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 var e = require('./email-validate');
 var d =new e();
-d.hello();
+d.isNotEmpty()
+
+console.log(d.validate("raj68518@gmail.com"));
+setTimeout( ()=>{
+    var dot = d.validate("ajfiuf")
+    console.log(dot);
+    
+}, 5000)
