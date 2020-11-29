e909f6beef2de8e2fce4e703c0f253ee72e771d2
raj68518 <raj68518@gmail.com>
2020-11-29 09:29:58 +0530

    primary check

--- email-validate.js
+++ email-validate.js
@@ -1,40 +1,18 @@
 
 class emailValidato{
-    emailId;
 
-     data = [];
-    
-    isNotEmpty(){
-        if (this.emailId != "" && this.emailId != "undefined") {
-            this.data.push(true);
-        }
-        this.data.push(false);
+    constructor(){
+        this.properties = [];
+    }
+    hi(){
+        this.properties.push(this.hi())
     }
 
-    minLength(length){
-        if (this.emailId.length >= length) {
-            return true;
-        }
-        return false;
-    }
-
-    maxLength(length){
-        if (this.emailId.length <= length) {
-            return true;
-        }
-        return false;
-    }
-
-    validate(emailId) {
-        this.emailId = emailId;
-        this.data.forEach(e => {
-            if (e === true) {
-                console.log("hi");
-                
-                return true
-            } else {
-                return false
-            }
+    helloHi(){
+        this.properties.forEach(e =>{
+           console.log(e);
+           
+            
         })
     }
 }
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var e = require('./email-validate');
 var d =new e();
-d.isNotEmpty()
+d.hi()
+d.helloHi();
 
-console.log(d.validate("raj68518@gmail.com"));
-setTimeout( ()=>{
-    var dot = d.validate("ajfiuf")
-    console.log(dot);
+// console.log(d.validate("raj68518@gmail.com"));
+// setTimeout( ()=>{
+//     var dot = d.validate("ajfiuf")
+//     console.log(dot);
     
-}, 5000)
+// }, 5000)
