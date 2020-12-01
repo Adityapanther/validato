f0560ba1a29afb60258cb32bd3f9010751c46c67
raj68518 <raj68518@gmail.com>
2020-12-01 14:47:45 +0530

    valid regex added for gmail or yahoo or hotail

--- index.js
+++ index.js
@@ -8,12 +8,3 @@ schema.onlyGmail();
 console.log(schema.validate("raj68518@gmail.com"));
 
 
-
-
-
-
-"raj68518@gmail.com"
-"aditya.kumar.802203@gmail.com"
-"adityar617@gmail.com"
-"teachnicalwithme@gmail.com"
-"aditya.&kumar.802202@gmail.com"
--- utlis/regex.js
+++ utlis/regex.js
@@ -1,4 +1,4 @@
-const defaultsPattern = "[a-zA-Z_0-9_.]*";
+const defaultsPattern = "[(\w\d\s)(!#$^&'*+-/=?_`{|}~)]*";
 const gMailPattern = defaultsPattern + "@gmail.com";
 const hotMailPattern = defaultsPattern + "@hotmail.com";
 const yahooMailPattern = defaultsPattern + "@yahoomail.com"
